2eca14f7d145fa7e523398424bf27253290d0314
gaga5lala <gaga5lala1013@gmail.com>
2017-11-10 22:39:34 +0800

    colorize

--- lint.rb
+++ lint.rb
@@ -1,21 +1,33 @@
 # TODO:
 #   1. error handling: file not exists, yaml format error, network connection error.
-#   2. linter display
 
 require 'json'
 require 'yaml'
 require 'faraday'
+require 'colorize'
 
 config_file = ARGV[0] || '.gitlab-ci.yml'
 
+print "Validation #{config_file}..."
+
 content = JSON.pretty_generate(YAML.load(File.read(config_file)))
 
-conn = Faraday.new(:url => 'https://gitlab.com')
+connection = Faraday.new(:url => 'https://gitlab.com') do |conn|
+  #conn.response :json, :content_type => /\bjson$/
+  conn.adapter Faraday.default_adapter
+end
 
-res = conn.post do |req|
+res = connection.post do |req|
   req.url '/api/v4/ci/lint'
   req.headers['Content-Type'] = 'application/json'
   req.body = { content: content }.to_json
 end
 
-p res
+res = JSON.parse(res.body)
+
+if res["status"] == 'valid'
+  puts "ok".colorize(:green)
+else
+  puts "invalid".colorize(:red)
+  puts res["errors"].to_s.colorize(:red)
+end
